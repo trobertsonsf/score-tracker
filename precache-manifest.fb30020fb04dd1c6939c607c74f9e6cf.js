f6d6f12416a9de96e7288ef1034fa820d3bb955c
Tim Robertson <trobertson@ojolabs.com>
2019-04-28 11:55:50 -0500

    Updates

---
+++ precache-manifest.fb30020fb04dd1c6939c607c74f9e6cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae2a1fa13bfd0c43c98c5cd9b60a411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7362035c60b0cbc1409",
+    "url": "/static/css/main.e5f6c731.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "e7362035c60b0cbc1409",
+    "url": "/static/js/main.adeb6561.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
